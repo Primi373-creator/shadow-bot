fdc0849dbcd8ff09e6cfd4fa5ef04e29ef1ce517
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 19:43:08 +0100

    sHADOW-MD

--- plugins/econ-balance.js
+++ plugins/econ-balance.js
@@ -1,23 +1,22 @@
-
 let handler = async (m, {conn, usedPrefix}) => {
 	
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let user = global.db.data.users[who]
-    if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
+    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
     conn.reply(m.chat, `
 ┌───⊷ *BALANCE* ⊶
-▢ *📌Nombre* : _@${who.split('@')[0]}_
-▢ *💎Diamantes* : _${user.diamond}_
-▢ *⬆️XP* : _Total ${user.exp}_
+✘ *📌Name* : _@${who.split('@')[0]}_
+✘ *💎Diamonds* : _${user.diamond}_
+✘ *⬆️XP* : _Total ${user.exp}_
 └──────────────
 
-*NOTA :* 
-Puedes comprar 💎 diamantes usando los comandos
-❏ *${usedPrefix}buy <cantidad>*
+*NOTE:* 
+You can buy 💎 diamonds using the commands
+❏ *${usedPrefix}buy <quantity>*
 ❏ *${usedPrefix}buyall*`, m, { mentions: [who] })
 }
 handler.help = ['balance']
 handler.tags = ['econ']
-handler.command = ['bal', 'diamantes', 'diamond', 'balance'] 
+handler.command = ['bal', 'diamonds', 'diamond', 'balance'] 
 
 export default handler
