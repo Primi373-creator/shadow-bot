3dfe32d61fdfcf7149da22168e07c856a5488ca8
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 18:42:12 +0100

    SHADOW-MD

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -2,18 +2,18 @@
 import fg from 'api-dylux' 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
  
- if (!args[0]) throw `✳️ Envíe el link de un video de Facebook\n\n📌 Ejemplo :\n*${usedPrefix + command}* https://fb.watch/d7nB8-L-gR/`
+ if (!args[0]) throw `✳️ Send the link of a Facebook video\n\n📌 Example:\n*${usedPrefix + command}* https://fb.watch/d7nB8-L-gR/`
     m.react(rwait)
    try {
     let result = await fg.fbdl(args[0]);
     let tex = `
-┌─⊷ *FBDL*
-▢ *Título:* ${result.title}
+📘 *Facebook Video Downloader*
+▣ *Title:* ${result.title}
 └───────────`;
     conn.sendFile(m.chat, result.videoUrl, 'fb.mp4', tex, m);
     m.react(done);
   } catch (error) {
- 	m.reply('Error: Intente de nuevo con otro link')
+ 	m.reply('Error: Try again with another link')
  	} 
 }
 handler.help = ['facebook'].map(v => v + ' <url>')
