96eba2ca934826a59a681eb989b85ee9aad7d128
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 18:54:59 +0100

    SHADOW-MD

--- plugins/dl-igstory.js
+++ plugins/dl-igstory.js
@@ -1,18 +1,18 @@
 
 import fg from 'api-dylux'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `✳️ Ingrese un nombre de usuario\n📌Ejemplo: *${usedPrefix + command}* auronplay`
+  if (!args[0]) throw `✳️ Enter a username\n📌 Example: *${usedPrefix + command}* auronplay`
   m.react(rwait)
   let res = await fg.igstory(args[0])
   for (let { url, type } of res.results) {
-    conn.sendFile(m.chat, url, 'igstory.bin', `✅ Historia de *${res.username}*`, m)
+    conn.sendFile(m.chat, url, 'igstory.bin', `✅ Story of *${res.username}*`, m)
   }
 
   m.react(done)
 }
 handler.help = ['igstory']
 handler.tags = ['dl']
-handler.command = ['igstory', 'ighistoria'] 
+handler.command = ['igstory'] 
 handler.diamond = true
 
 export default handler
