8dd40df498bf695d4a8076275866f369d8ef649d
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 20:04:16 +0100

    SHADOW-MD

--- plugins/econ-rob.js
+++ plugins/econ-rob.js
@@ -2,27 +2,27 @@
 let ro = 3000
 let handler = async (m, { conn, usedPrefix, command}) => {
     let time = global.db.data.users[m.sender].lastrob + 7200000
-    if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `⏱️¡Hey! Espera *${msToTime(time - new Date())}* para volver a robar`
+    if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `⏱️Chotto mattae *${msToTime(time - new Date())}* to rob again`
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
-    if (!who) throw `✳️ Etiqueta a alguien para robar`
-    if (!(who in global.db.data.users)) throw `✳️ El usuario no se encuentra en mi base de datos`
+    if (!who) throw `✳️ Tag someone to rob`
+    if (!(who in global.db.data.users)) throw `✳️ User is not in my database`
     let users = global.db.data.users[who]
     let rob = Math.floor(Math.random() * ro)
-    if (users.exp < rob) return m.reply(`🔖 @${who.split`@`[0]} tiene menos de *${ro} xp*\nNo robes a un podre v":`, null, { mentions: [who] })    
+    if (users.exp < rob) return m.reply(`🔖 @${who.split`@`[0]} has less than *${ro} xp*\nDon't rob a poor person":`, null, { mentions: [who] })    
    global.db.data.users[m.sender].exp += rob
    global.db.data.users[who].exp -= rob
   
     m.reply(`
-  ‣ Robaste *${rob} XP* a @${who.split`@`[0]}
+  ‣ You robbed *${rob} XP* from @${who.split`@`[0]}
   `, null, { mentions: [who] })
     global.db.data.users[m.sender].lastrob = new Date * 1
   }
 
   handler.help = ['rob']
   handler.tags = ['econ']
-  handler.command = ['robar', 'rob']
+  handler.command = ['rob <user>', 'rob']
   
   export default handler
   
@@ -36,6 +36,6 @@ let handler = async (m, { conn, usedPrefix, command}) => {
     minutes = (minutes < 10) ? "0" + minutes : minutes
     seconds = (seconds < 10) ? "0" + seconds : seconds
   
-    return hours + " Hora(s) " + minutes + " Minuto(s)"
+    return hours + " Hour(s) " + minutes + " Minute(s)"
   }
   
