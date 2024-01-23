251b1284a51fc22ea47e9cbb33bbeb9a888fda14
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 18:51:30 +0100

    SHADOW-MD

--- plugins/dl-igstalk.js
+++ plugins/dl-igstalk.js
@@ -2,22 +2,22 @@
 import fg from 'api-dylux'
 let handler= async (m, { conn, args, text, usedPrefix, command }) => {
 	
-    if (!args[0]) throw `✳️ Ingrese el Username de Instagram\n\n📌Ejemplo: ${usedPrefix + command} fg98_ff` 
+    if (!args[0]) throw `✳️ Enter the Instagram Username\n\n📌 Example: ${usedPrefix + command} ronaldo` 
     try {
     let res = await fg.igStalk(args[0])
     let te = `
 ┌──「 *STALKING* 
-▢ *🔖Nombre:* ${res.name} 
-▢ *🔖Username:* ${res.username}
-▢ *👥Seguidores:* ${res.followersH}
-▢ *🫂Siguiendo:* ${res.followingH}
-▢ *📌Bio:* ${res.description}
-▢ *🏝️Posts:* ${res.postsH}
-▢ *🔗 Link* : https://instagram.com/${res.username.replace(/^@/, '')}
+✘ *🔖Name:* ${res.name} 
+✘ *🔖Username:* ${res.username}
+✘ *👥followers:* ${res.followersH}
+✘ *🫂Following:* ${res.followingH}
+✘ *📌Bio:* ${res.description}
+✘ *🏝️Posts:* ${res.postsH}
+✘ *🔗 Link* : https://instagram.com/${res.username.replace(/^@/, '')}
 └────────────`
      await conn.sendFile(m.chat, res.profilePic, 'igstalk.png', te, m)
       } catch {
-        m.reply(`✳️ Revisa que el nombre de usuario sea de *Instagram*`)
+        m.reply(`✳️ Check that the username is from *Instagram*`)
       }
 }
 handler.help = ['igstalk']
