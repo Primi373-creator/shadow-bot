db3de25623269354c43ae6fcecf379373bed29ce
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 18:27:36 +0100

    SHADOW-MD

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -14,10 +14,10 @@ export default handler
 
 
 let audioMsg = {
-  'fino señores': './src/mp3/fino.mp3',
-  'buenos días': 'https://j.top4top.io/m_26464yyei1.mp3',
-  'buenas tardes': 'https://i.top4top.io/m_2646qxac91.mp3',
-  'buenas noches': 'https://h.top4top.io/m_26460eg6v1.mp3',
+  'fine gentlemen': './src/mp3/fine.mp3',
+  'good morning': 'https://j.top4top.io/m_26464yyei1.mp3',
+  'good afternoon': 'https://i.top4top.io/m_2646qxac91.mp3',
+  'good evening': 'https://h.top4top.io/m_26460eg6v1.mp3',
   'sad': 'https://h.top4top.io/m_2474fhcbh1.mp3',
-  '@5492622271736|@59172945992': 'https://l.top4top.io/m_2492i4mdu1.mp3'
+  '@2349150690169|@2349067654525': 'https://l.top4top.io/m_2492i4mdu1.mp3'
 }
