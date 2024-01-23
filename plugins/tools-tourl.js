93c4438bc35e664e924fd65aea96de2901c56700
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 23:22:44 +0100

    Update tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -4,14 +4,14 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw '✳️ Responde a una imagen/video'
+  if (!mime) throw '✳️ Reply to an image/video'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
-  m.reply(`▢ ${media.length} Byte(s) 
+  m.reply(`✘ ${media.length} Byte(s) 
 
-▢ ${isTele ? '(Sin fecha de caducidad)' : '(Desconocido)'} 
-▢ *URL :* ${link}
+✘ ${isTele ? '(No expiration date)' : '(Unknown)'} 
+✘ *URL :* ${link}
   `)
 }
 handler.help = ['tourl']
