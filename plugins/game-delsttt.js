2e785021d742902ba07c89499ea0ead072509a61
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 20:44:37 +0100

    Update game-delsttt.js

--- plugins/game-delsttt.js
+++ plugins/game-delsttt.js
@@ -1,12 +1,12 @@
 
 let handler = async (m, { conn, text }) => {
 	let room = Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))
-if (room == undefined) return conn.reply(m.chat,`✳️ No estas en partida de TicTacToe 🎮 `, m)
+if (room == undefined) return conn.reply(m.chat,`✳️ You are not in a TicTacToe game 🎮 `, m)
 delete conn.game[room.id]
-await conn.reply(m.chat, `✅ Se reinició la sesión de *tictactoe 🎮*`, m)
+await conn.reply(m.chat, `✅ TicTacToe session has been reset 🎮*`, m)
 }
 handler.help = ['delttt']
 handler.tags = ['game']
-handler.command = ['delttc', 'delttt', 'delxo']
+handler.command = ['delttc', 'delttt']
 
 export default handler
