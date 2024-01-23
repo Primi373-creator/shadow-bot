54ff5f328a8af5801e294328cd590bec8ea5aabd
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 19:06:09 +0100

    SHADOW-MD

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -2,20 +2,20 @@
 import yts from 'yt-search'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
 	
-	if (!text) throw `✳️ Ingresa el título de una canción\n\n📌Ejemplo *${usedPrefix + command}* Lil Peep hate my life`
+	if (!text) throw `✳️ Enter the title of a song\n\n📌Example *${usedPrefix + command}* Nf why`
 	let res = await yts(text)
 	let vid = res.videos[0]
-	if (!vid) throw `✳️ Vídeo/Audio no encontrado`
+	if (!vid) throw `✳️ Video/Audio not found`
 	let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
 	//const url = 'https://www.youtube.com/watch?v=' + videoId
 	m.react('🎧')
 	let play = `
 	≡ *FG MUSIC*
 ┌──────────────
-▢ 📌 *Título* : ${title}
-▢ 📆 *Publicado:* ${ago}
-▢ ⌚ *Duración:* ${timestamp}
-▢ 👀 *Vistas:* ${views}
+✘ 📌 *Title* : ${title}
+✘ 📆 *Published:* ${ago}
+✘ ⌚ *Duration:* ${timestamp}
+✘ 👀 *Views:* ${views}
 └──────────────`
  await conn.sendButton(m.chat, play, fgig, thumbnail, [
     ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
