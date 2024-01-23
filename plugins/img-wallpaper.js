ac53c8e6edf601a8edefd5fa0e75f5edf59553d2
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 21:38:32 +0100

    Update img-wallpaper.js

--- plugins/img-wallpaper.js
+++ plugins/img-wallpaper.js
@@ -1,13 +1,13 @@
 
 import fg from 'api-dylux';
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `✳️ Que fondo de pantalla busco?`
+  if (!text) throw `✳️ What wallpaper are you looking for?`
   try {
     let res = await fg.wallpaper(text);
     let re = pickRandom(res);
-    await conn.sendMessage(m.chat, { image: { url: re.image[0] }, caption: `✅ Genial no?` }, { quoted: m });
+    await conn.sendMessage(m.chat, { image: { url: re.image[0] }, caption: `✅ Great, isn't it?` }, { quoted: m });
   } catch (error) {
-   m.reply(`✳️ Error: intenta más tarde`)
+   m.reply(`✳️ Error: Please try again later`)
   }
   
 }
