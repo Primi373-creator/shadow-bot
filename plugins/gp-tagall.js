75ffe457d4a073afeb7f6f37636d2369fa1fd92e
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 21:28:16 +0100

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`▢ Grupo : *${groupMetadata.subject}*\n▢ Miembros : *${participants.length}*${text ? `\n▢ Mensaje : ${text}\n` : ''}\n┌───⊷ *MENCIONES*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ Dylux ┃ ᴮᴼᵀ ✪──', null, {
+    m.reply(`✘ Group : *${groupMetadata.subject}*\n✘ Members : *${participants.length}*${text ? `\n✘ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '✘ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ GURU ┃ ᴮᴼᵀ ✪──', null, {
         mentions: users
     })
 }
