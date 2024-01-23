3a32e2b1a098fd788fbd9225511a738e74681b19
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 19:16:04 +0100

    SHADOW-MD

--- plugins/dl-playlist.js
+++ plugins/dl-playlist.js
@@ -3,18 +3,18 @@ import { youtubeSearch } from '@bochilteam/scraper'
 import yts from 'yt-search'
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 
-    if (!text) throw `✳️ Ingresa el título de una canción\n\n*📌 Ejemplo*\n*${usedPrefix + command}* Lil Peep hate my fuccn life `
+    if (!text) throw `✳️ Enter the title of a song\n\n*📌 Example*\n*${usedPrefix + command}* NF WHY `
     m.react('📀')
     let result = await yts(text)
     let ytres = result.videos
     let listSections = []
 	Object.values(ytres).map((v, index) => {
 	listSections.push([`${index}┃ ${v.title}`, [
-          ['🎶 MP3', `${usedPrefix}fgmp3 ${v.url}`, `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título* : ${v.title}\n▢ 📆 *Publicado:* ${v.ago}\n`],
-          ['🎥 MP4', `${usedPrefix}fgmp4 ${v.url}`, `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título* : ${v.title}\n▢ 📆 *Publicado:* ${v.ago}\n`]
+          ['🎶 MP3', `${usedPrefix}fgmp3 ${v.url}`, `✘ ⌚ *Duration:* ${v.timestamp}\n✘ 👀 *Views:* ${v.views}\n✘ 📌 *Title* : ${v.title}\n✘ 📆 *Published:* ${v.ago}\n`],
+          ['🎥 MP4', `${usedPrefix}fgmp4 ${v.url}`, `✘ ⌚ *Duration:* ${v.timestamp}\n✘ 👀 *Views:* ${v.views}\n✘ 📌 *Title* : ${v.title}\n✘ 📆 *Published:* ${v.ago}\n`]
         ]])
 	})
-	return conn.sendList(m.chat, '  ≡ *FG MUSIC*🔎', `\n 📀 Aqui una lista de resultados de :\n *${text}*`, fgig, `Click Aquí `, listSections, m)
+	return conn.sendList(m.chat, '  ≡ *MUSIC DOWNLOADER*🔎', `\n 📀Here is a list of results for :\n *${text}*`, fgig, `Click Here `, listSections, m)
 }
 handler.help = ['play2']
 handler.tags = ['dl']
