2ee33123792be425a602e84d118783bbef06c5c4
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 20:43:29 +0100

    Update fun-txt.js

--- plugins/fun-txt.js
+++ plugins/fun-txt.js
@@ -2,10 +2,10 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-     if (!teks) throw `📝 Que escribo? Ejemplo : *${usedPrefix + command}* Hola puercos`
+     if (!teks) throw `📝 What should I write? Example: *${usedPrefix + command}* Hello pigs`
       m.react(rwait)
       let img = global.API('fgmods', '/api/maker/txt', { text: teks }, 'apikey')
-      conn.sendFile(m.chat, img, 'img.png', `✅ Es mejor de lo que escribes tú ✍🏻`, m)
+      conn.sendFile(m.chat, img, 'img.png', `✅ It's better than what you wrote ✍🏻`, m)
       m.react(done)
 
   }
