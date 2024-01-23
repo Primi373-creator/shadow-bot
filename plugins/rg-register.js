696dd86e72082d12974cd01744c26ddbbf936634
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 22:59:23 +0100

    Update rg-register.js

--- plugins/rg-register.js
+++ plugins/rg-register.js
@@ -1,38 +1,38 @@
+//import db from '../lib/database.js'
 
 import { createHash } from 'crypto'
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) throw `✳️ Ya estás registrado\n\n¿Quiere volver a registrarse?\n\n 📌 Use este comando para eliminar su registro \n*${usedPrefix}unreg* <Número de serie>`
-  if (!Reg.test(text)) throw `⚠️ Formato incorrecto\n\n ✳️ Uso del comamdo: *${usedPrefix + command} nombre.edad*\n📌Ejemplo : *${usedPrefix + command}* ${name2}.16`
+  if (user.registered === true) throw `✳️ You are already registered\n\nDo you want to re-register?\n\n 📌 Use this command to remove your record \n*${usedPrefix}unreg* <Serial number>`
+  if (!Reg.test(text)) throw `⚠️ Format incorrect\n\n ✳️ Use this command: *${usedPrefix + command} name.age*\n📌Exemple : *${usedPrefix + command}* ${name2}.16`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw '✳️ El nombre no puede estar vacío'
-  if (!age) throw '✳️ La edad no puede estar vacía'
-  if (name.length >= 30) throw '✳️ El nombre es demasiado largo' 
+  if (!name) throw '✳️ The name cannot be empty'
+  if (!age) throw '✳️ age cannot be empty'
+  if (name.length >= 30) throw '✳️The name is too long' 
   age = parseInt(age)
-  if (age > 100) throw '👴🏻 Wow el abuelo quiere jugar al bot'
-  if (age < 5) throw '🚼  hay un abuelo bebé jsjsj '
+  if (age > 100) throw '👴🏻 Wow grandpa wants to play bot'
+  if (age < 5) throw '🚼  there is a grandpa baby jsjsj '
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-┌─「 *REGISTRADO* 」─
-▢ *Nombre:* ${name}
-▢ *Edad* : ${age} años
-▢ *Numero de serie* :
+╭━━〘 *REGISTERED* 〙───⊷
+┃ ✘ *NUMBER:* ${name}
+┃ ✘ *AGE* : ${age} years
+┃ ✘ *SERIEL NUMBER* :
 ${sn}
-└──────────────
+╰━━━━━━━━━━━━━━⊷
 
- *${usedPrefix}help* para ver el Menu
+ *${usedPrefix}help* to see menu
 `.trim())
 }
-handler.help = ['reg'].map(v => v + ' <nombre.edad>')
+handler.help = ['reg'].map(v => v + ' <name.age>')
 handler.tags = ['rg']
 
 handler.command = ['verify', 'reg', 'register', 'registrar'] 
 
 export default handler
-
