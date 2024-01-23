2ec4e927a03eeb9ab9da69ed90ea8fe2db942527
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 21:47:54 +0100

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -2,15 +2,11 @@
 let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
-≡ *DONACION*
-puedes donar si quieres ayudar a mantener el bot activo
 
-▢ *PayPal*
-• *Link :* https://paypal.me/fg98f
+≡ DONATION
+You can contribute if you want to help keep the bot active
 
-▢ *Mercado Pago Arg*
-• *Alias :* fg98ff
-• *N° :* 1168352204
+✘*whatsapp me @2349150690169
 `
 let img = 'https://i.ibb.co/37FP2bk/donate.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
@@ -18,6 +14,6 @@ conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 handler.help = ['donate']
 handler.tags = ['main']
-handler.command = ['apoyar', 'donate', 'donar'] 
+handler.command = ['donate'] 
 
 export default handler
