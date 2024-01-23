5a5e4554c538d40ce301e6de3e8c21f7a59716e9
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 23:00:15 +0100

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -2,12 +2,12 @@
 
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) throw `✳️ *Ingrese número de serie*\nVerifique su número de serie con el comando...\n\n*${usedPrefix}nserie*`
+  if (!args[0]) throw `✳️ *Enter serial number*\nCheck your serial number with the command...\n\n*${usedPrefix}nserie*`
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw '⚠️ *Número de serie incorrecto*'
+  if (args[0] !== sn) throw '⚠️ *Incorrect serial number*'
   user.registered = false
-  m.reply(`✅ Registro eliminado`)
+  m.reply(`✅ Register eliminated`)
 }
 handler.help = ['unreg <Num Serie>'] 
 handler.tags = ['rg']
@@ -16,4 +16,3 @@ handler.command = ['unreg']
 handler.register = true
 
 export default handler
-
