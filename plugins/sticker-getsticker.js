c3f8059f6356107089353afb89d0d63a17671886
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 23:06:07 +0100

    Update sticker-getsticker.js

--- plugins/sticker-getsticker.js
+++ plugins/sticker-getsticker.js
@@ -3,32 +3,32 @@ import fg from 'api-dylux'
 import fetch from 'node-fetch'
 import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-    if (!args[0]) throw `✳️ ingrese lo que quiere buscar \n\n📌*Ejemplo:*\n${usedPrefix + command} homero`
+    if (!args[0]) throw `✳️ enter what you want to search \n\n📌*Example:*\n${usedPrefix + command} homero`
     
-    //Resultados de https://getstickerpack.com/
+    //Result https://getstickerpack.com/
     try {
    /*let res = await fetch(global.API('fgmods', '/api/getsticker', { q:text }, 'apikey'))
    let json = await res.json()*/
    let json = await fg.StickerSearch(text) 
     m.reply(`
-✅ Resultado
+✅ Result
 
-▢ *Titulo:* ${json.title}
-▢ *Total stickers:* ${json.sticker_url.length}
-▢ *Tiempo estimado de envio:* _*${json.sticker_url.length * 2} s*_`)
+✘ *Title:* ${json.title}
+✘ *Total stickers:* ${json.sticker_url.length}
+✘ *Estimated shipping time:* _*${json.sticker_url.length * 2} s*_`)
     for (let i of json.sticker_url) {
         const stiker = await sticker(false, i, global.packname, global.author)
         await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
         //await delay(1500)
     }
     } catch (e) {
-	m.reply(`❇️ Error: prueba con otro`)
+	m.reply(`❇️ Error: try another`)
 	} 
 }
 handler.help = ['getsticker']
 handler.tags = ['sticker']
 handler.command = ['getsticker', 'getstick', 'stickersearch', 'sticksearch'] 
-handler.diamond = 3
+handler.diamond = false
 
 export default handler
 
