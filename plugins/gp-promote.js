54b47fd82ea1313e2c84481d1740e1596c143d23
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 21:23:13 +0100

    Update gp-promote.js

--- plugins/gp-promote.js
+++ plugins/gp-promote.js
@@ -6,8 +6,8 @@ var number = text.split`@`[1]
 }else if(!isNaN(text)) {
 var number = text
 }
-if(!text && !m.quoted) return conn.reply(m.chat, `✳️ Uso del comamdo \n *${usedPrefix + command}* @tag  (o responda un mensaje)`, m)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `✳️ Numero incorrecto`, m)
+if(!text && !m.quoted) return conn.reply(m.chat, `✳️ Using the command \n *${usedPrefix + command}* @tag  (or reply to a message)`, m)
+if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `✳️ Number incorrect`, m)
 try {
 if(text) {
 var user = number + '@s.whatsapp.net'
@@ -19,7 +19,7 @@ var user = number + '@s.whatsapp.net'
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-m.reply(`✅ Usuario promovido`)
+m.reply(`✅ User promoted`)
 }}
 handler.help = ['promote']
 handler.tags = ['group']
