d0311c3b2d5a5364f16e6defd6e09df4cd335049
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 21:06:13 +0100

    Update game-tictactoe.js

--- plugins/game-tictactoe.js
+++ plugins/game-tictactoe.js
@@ -1,16 +1,24 @@
-import TicTacToe from '../lib/tictactoe.js'
+import TicTacToe from '../lib/tictactoe.js';
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-    conn.game = conn.game ? conn.game : {}
-    if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) throw `✳️ Todavía estás en el juego para reiniciar la session escribe : *${usedPrefix}delttt*`
-    if (!text) throw `✳️ Ponga un nombre a la sala`
-    let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
-    // m.reply('[WIP Feature]')
+    conn.game = conn.game ? conn.game : {};
+
+    if (Object.values(conn.game).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) {
+        throw `✳️ You are still in the game. To restart the session, type: *${usedPrefix}delttt*`;
+    }
+
+    if (!text) {
+        throw `✳️ Please provide a name for the room.`;
+    }
+
+    let room = Object.values(conn.game).find(room => room.state === 'WAITING' && (text ? room.name === text : true));
+
     if (room) {
-        m.reply('✅ Compañero encontrado')
-        room.o = m.chat
-        room.game.playerO = m.sender
-        room.state = 'PLAYING'
+        m.reply('✅ Partner found.');
+        room.o = m.chat;
+        room.game.playerO = m.sender;
+        room.state = 'PLAYING';
+
         let arr = room.game.render().map(v => {
             return {
                 X: '❎',
@@ -24,27 +32,31 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
                 7: '7️⃣',
                 8: '8️⃣',
                 9: '9️⃣',
-            }[v]
-        })
+            }[v];
+        });
+
         let str = `
-Esperando a @${room.game.currentTurn.split('@')[0]} como primer jugador
-        
+Waiting for @${room.game.currentTurn.split('@')[0]} as the first player
+
 ${arr.slice(0, 3).join('')}
 ${arr.slice(3, 6).join('')}
 ${arr.slice(6).join('')}
 
-▢ *SALA ID* ${room.id}
+✘ *ROOM ID* ${room.id}
 
-▢ *Reglas*
-‣ Haz 3 filas de símbolos verticales, horizontales o diagonales para ganar
-‣ Escribe *surrender* para salir del juego y ser declarado derrotado.
-`.trim()
+✘ *Rules*
+‣ Make 3 rows of symbols vertically, horizontally, or diagonally to win
+‣ Type *surrender* to exit the game and be declared defeated.
+`.trim();
+
         if (room.x !== room.o) await conn.reply(room.x, str, m, {
             mentions: conn.parseMention(str)
-        })
+        });
+
         await conn.reply(room.o, str, m, {
             mentions: conn.parseMention(str)
-        })
+        });
+
     } else {
         room = {
             id: 'tictactoe-' + (+new Date),
@@ -52,23 +64,23 @@ ${arr.slice(6).join('')}
             o: '',
             game: new TicTacToe(m.sender, 'o'),
             state: 'WAITING'
-        }
-        if (text) room.name = text
-        
-     conn.reply(m.chat, `⏳ *Esperando pareja*\nEscriba el siguiente comando para aceptar
-▢ *${usedPrefix + command} ${text}*
+        };
 
-🎁 Recompensa: *4999 XP*`, m, {
+        if (text) room.name = text;
+
+        conn.reply(m.chat, `⏳ *Waiting for a partner*\nType the following command to accept
+✘ *${usedPrefix + command} ${text}*
+
+🎁 Reward: *4999 XP*`, m, {
             mentions: conn.parseMention(text)
-        })
-        
-   conn.game[room.id] = room
+        });
+
+        conn.game[room.id] = room;
     }
-    
-}
+};
 
-handler.help = ['tictactoe <nombre Sala>']
-handler.tags = ['game']
-handler.command = ['tictactoe', 'ttc', 'ttt', 'xo']
+handler.help = ['tictactoe <Room Name>'];
+handler.tags = ['game'];
+handler.command = ['tictactoe', 'ttc', 'ttt', 'xo'];
 
-export default handler
+export default handler;
