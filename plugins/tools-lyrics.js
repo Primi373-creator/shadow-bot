a7e640ee9f9626f7c68c38adbf805e3822e8c9b7
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 23:18:38 +0100

    Update tools-lyrics.js

--- plugins/tools-lyrics.js
+++ plugins/tools-lyrics.js
@@ -2,13 +2,13 @@
 import fg from 'api-dylux'
 let handler = async (m, {conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-   if (!teks) throw `✳️ Ingresa el título de una canción`
+  if (!teks) throw `✳️ Enter the title of a song`
    try {
  let res = await fg.lyrics(text);
- let mes = `▢ *${res.title}*
-▢ *${res.artist}*
+ let mes = `✘ *${res.title}*
+✘ *${res.artist}*
 
-▢ ${res.lyrics}`;
+✘ ${res.lyrics}`;
     conn.sendFile(m.chat, res.thumb, 'img.png', mes, m);
 } catch (e) {
 	m.react(error)
