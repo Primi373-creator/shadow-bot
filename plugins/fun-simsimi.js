2a57694840f3755726fdb361f2057f0953ce4b3f
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 20:35:03 +0100

    SHADOW-MD

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 	
  let name = conn.getName(m.sender)
-  if (!text) throw `Hola *${name}* Quieres charlar un rato? \nResponde con *${usedPrefix + command}* (tu mensaje) \n\n📌 Ejemplo : *${usedPrefix + command}* Hola bot`
+  if (!text) throw `Hello *${name}*! Do you want to chat for a while? \nReply with *${usedPrefix + command}* (your message) \n\n📌 Example: *${usedPrefix + command}* Hello bot`
   m.react('🗣️') 
   //let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: "es" }, ''))
   let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=es`)
