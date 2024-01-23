3ca30be7426e62b6789333740288b252a37059f1
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 22:38:15 +0100

    Update owner-addexpired.js

--- plugins/owner-addexpired.js
+++ plugins/owner-addexpired.js
@@ -1,6 +1,6 @@
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0] || isNaN(args[0])) throw `✳️ Ingrese un número que represente el número de días!\n\n📌 Ejemplo :\n*${usedPrefix + command}* 30`
+    if (!args[0] || isNaN(args[0])) throw `✳️ Please enter a number representing the number of days!\n\n📌 Example:\n*${usedPrefix + command}* 30`
 
     let who
     if (m.isGroup) who = args[1] ? args[1] : m.chat
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     var now = new Date() * 1
     if (now < global.db.data.chats[who].expired) global.db.data.chats[who].expired += nDays
     else global.db.data.chats[who].expired = now + nDays
-    let teks = `✅ Se estableció los días de vencimiento para \n*${await conn.getName(who)}* \n\n*Durante:* ${args[0]} Días\n\n*Cuenta regresiva :* ${msToDate(global.db.data.chats[who].expired - now)}`
+    let teks = `✅ Expiry days set for\n*${await conn.getName(who)}*\n\n*For:* ${args[0]} Days\n\n*Countdown:* ${msToDate(global.db.data.chats[who].expired - now)}`
     conn.reply(m.chat, teks, m)
 }
 handler.help = ['expired <días>']
@@ -24,5 +24,5 @@ function msToDate(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' *Días*\n ', h, ' *Horas*\n ', m, ' *Minutos*\n ', s, ' *Segundos* '].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' *Days*\n ', h, ' *Hours*\n ', m, ' *Minutes*\n ', s, ' *Seconds* '].map(v => v.toString().padStart(2, 0)).join('')
 }
