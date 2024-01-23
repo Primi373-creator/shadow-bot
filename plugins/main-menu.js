18137b43154364baced37e25941ba23638264385
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 23:30:36 +0100

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -143,9 +143,9 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     
-    let pp = './src/fg_logo.jpg'
+    let pp = './src/shadow.mp4'
      
-    conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m, null, rpl)
+    conn.sendFile(m.chat, pp, 'menu.mp4', text.trim(), m, null, rpl)
     /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff', pp, [
       ['ꨄ︎ Apoyar', `${_p}donate`],
       ['⏍ Info', `${_p}botinfo`],
