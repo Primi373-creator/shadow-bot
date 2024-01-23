2b31ba8118d6265f87eb73924e6199e99baac6b1
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-23 18:21:03 +0100

    SHADOW-MD

--- plugins/anime-rkill.js
+++ plugins/anime-rkill.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	 let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
-    if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @tag` 
+    if (!who) throw `✳️ Mention or tag someone\n\n📌 Ejemplo : ${usedPrefix + command} @tag` 
     
     let user = global.db.data.users[who]
     let name = conn.getName(who) 
@@ -18,15 +18,15 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!rki.ok) throw await rki.text()
    let jkis = await rki.json()
    let { url } = jkis
-   let stiker = await sticker(null, url, `(${name2}) mató a`, `${name}`)
+   let stiker = await sticker(null, url, `(${name2}) killed `, `${name}`)
    conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
    m.react('🗡️') 
    
 }
 
 handler.help = ['kill @tag']
-handler.tags = ['rnime']
-handler.command = /^(kill|matar)$/i
+handler.tags = ['killuser']
+handler.command = /^(kill)$/i
 handler.diamond = true
 handler.group = true
 
