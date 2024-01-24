4edd888e78db756e7b4e98acdc5ad7fce1668b96
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-01-24 02:54:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,53 +6,29 @@ import { toBuffer } from 'qrcode'
 import fetch from 'node-fetch'
 
 function connect(conn, PORT) {
-    let app = global.app = express()
-    console.log(app)
-    let server = global.server = createServer(app)
-    // app.use(express.static(path.join(__dirname, 'views')))
-    let _qr = 'invalid'
+    let app = global.app = express();
+    let server = global.server = createServer(app);
+    let _qr = 'invalid';
 
     conn.ev.on('connection.update', function appQR({ qr }) {
-        if (qr) _qr = qr
-    })
+        if (qr) _qr = qr;
+    });
 
-    app.use(async (req, res) => {
-        res.setHeader('content-type', 'image/png')
-        res.end(await toBuffer(_qr))
-    })
-
-    // let io = new Socket(server)
-    // io.on('connection', socket => {
-    //     let { unpipeEmit } = pipeEmit(conn, socket, 'conn-')
-    //     socket.on('disconnect', unpipeEmit)
-    // })
+    app.get('/', (req, res) => {
+        // Render a simple HTML page with the QR code and text
+        res.send(`
+            <html>
+                <body>
+                    <img src="${_qr}" alt="QR Code">
+                    <p>hello from Cipher.</p>
+                </body>
+            </html>
+        `);
+    });
 
     server.listen(PORT, () => {
-        console.log('App listened on port', PORT)
-        if (opts['keepalive']) keepAlive()
-    })
+        console.log('App listened on port', PORT);
+    });
 }
 
-function pipeEmit(event, event2, prefix = '') {
-    let old = event.emit
-    event.emit = function (event, ...args) {
-        old.emit(event, ...args)
-        event2.emit(prefix + event, ...args)
-    }
-    return {
-        unpipeEmit() {
-            event.emit = old
-        }
-    }
-}
-
-function keepAlive() {
-    const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
-    if (/(\/\/|\.)undefined\./.test(url)) return
-    setInterval(() => {
-        fetch(url).catch(console.error)
-    }, 5 * 1000 * 60)
-}
-
-
-export default connect
+export default connect;
